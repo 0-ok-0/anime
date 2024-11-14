e7ced22dbc7dfc6db3a03013bc3a5459bc3bfffa
0-ok-0 <azattnurr76@gmail.com>
2024-11-14 20:58:09 +0300

    1

--- animaut/animaut/urls.py
+++ animaut/animaut/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from amime.views import index
+from animaut.anime import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('anime/', index),
+    path('anime/', views.index),
 ]
